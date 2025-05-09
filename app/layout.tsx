30aeff80d6fe397fb71177982ef941f4c89fed97
WilliamMeinhardt <willmein10@gmail.com>
2025-05-09 14:17:44 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import CookieConsent from "@/components/CookieConsent"
 import Script from "next/script"
-
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,12 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="no">
+      <head>
+        <Script
+          id="cookie-consent"
+          src="https://policy.app.cookieinformation.com/uc.js"
+          data-culture="NB"
+          data-gcm-version="2.0"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body className={`${inter.className} bg-[#f5f2ed]`}>
-        
         <Header />
         <main>{children}</main>
         <Footer />
-       <Script id="CookieConsent" src="https://policy.app.cookieinformation.com/uc.js" data-culture="NB" data-gcm-version="2.0" type="text/JavaScript" />
+        <CookieConsent />
       </body>
     </html>
   )
